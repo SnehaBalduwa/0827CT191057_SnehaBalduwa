cb6847041bf460b7102fe2ae8bbe5229e6d90a38
SnehaBalduwa <91717862+SnehaBalduwa@users.noreply.github.com>
2021-10-12 23:10:26 +0530

    Create program10.py

---
+++ program10.py
@@ -0,0 +1,14 @@
+myDict = {
+
+    "Kitab" : "Book", 
+    "Dimag" : "Brain",
+    "Aacha" : "Good",
+    "Dil" : "Heart",
+    "Galti" : "Mistake",
+}
+
+print("Options are : ", myDict.keys())
+
+a = input("Enter Your Word : ")
+
+print("The meaning of your word is : ", myDict[a])
